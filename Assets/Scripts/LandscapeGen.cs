ef9eb64fa3012a2ffd6d79d8fdf701d830f167c5
thomz12 <thomz12@hotmail.nl>
2016-08-27 20:39:23 +0200

    TerrainGen

--- Assets/Scripts/LandscapeGen.cs
+++ Assets/Scripts/LandscapeGen.cs
@@ -9,9 +9,12 @@ public class LandscapeGen : MonoBehaviour {
 
     public int detail;
     public int roughness;
-    public const float size = 480;
+    public int minHeight;
+    public int maxHeight;
 
-    private float oldHeight = -2.70f;
+    public const float size = 100;
+
+    private float oldHeight = -2.5f;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +23,11 @@ public class LandscapeGen : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (GameObject.FindGameObjectWithTag("MainCamera").transform.position.x >= 190 + 480 * spawnCount)
+        if (GameObject.FindGameObjectWithTag("MainCamera").transform.position.x >= size * spawnCount)
         {
             GameObject go = (GameObject)Instantiate(Landscape);
             spawnCount++;
-            go.transform.position = new Vector3(480 * spawnCount, 0, 0);
+            go.transform.position = new Vector3(size * spawnCount, 0, 0);
             go.transform.SetParent(this.gameObject.transform);
 
             Mesh m = new Mesh();
@@ -32,16 +35,29 @@ public class LandscapeGen : MonoBehaviour {
             float stepsize = size / detail;
 
             List<Vector3> verts = new List<Vector3>();
+            List<Vector2> vert2d = new List<Vector2>();
             List<int> indicies = new List<int>();
 
             for (int i = 0; i < detail; i++)
             {
-                float newHeight = Random.Range(-10f, 10f);
+                float delta = Random.Range(-roughness, roughness);
 
-                verts.Add(new Vector3(i * stepsize, oldHeight, 0));
-                verts.Add(new Vector3(i * stepsize, -10, 0));
-                verts.Add(new Vector3(i * stepsize + stepsize, newHeight, 0));
-                verts.Add(new Vector3(i * stepsize + stepsize, -10, 0));
+                float newHeight = oldHeight + delta;
+
+                if (newHeight < minHeight)
+                    newHeight = minHeight;
+                if (newHeight > maxHeight)
+                    minHeight = maxHeight;
+
+                vert2d.Add(new Vector2(i * stepsize, oldHeight));
+                vert2d.Add(new Vector2(i * stepsize, -10));
+                vert2d.Add(new Vector2(i * stepsize + stepsize, newHeight));
+                vert2d.Add(new Vector2(i * stepsize + stepsize, -10));
+
+                verts.Add(vert2d[i]);
+                verts.Add(vert2d[i + 1]);
+                verts.Add(vert2d[i + 2]);
+                verts.Add(vert2d[i + 3]);
 
                 oldHeight = newHeight;
 
@@ -63,7 +79,10 @@ public class LandscapeGen : MonoBehaviour {
             go.AddComponent<MeshRenderer>();
             go.AddComponent<MeshFilter>();
             go.GetComponent<MeshFilter>().mesh = m;
-            go.transform.position = new Vector3(size * spawnCount, 0, 5);
+            go.transform.position = new Vector3(size * spawnCount - size / 2, 0, 5);
+            go.AddComponent<PolygonCollider2D>();
+
+            go.GetComponent<PolygonCollider2D>().points = vert2d.ToArray();
         }
 	}
 }
