d93aa261b3f9dffccaf091c68037b4d14eea7ff1
remco96 <remcostolwijk1996@gmail.com>
2016-08-27 13:14:55 +0200

    egg break, TODO, Game state

---
+++ Assets/Scripts/Egg.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Egg : MonoBehaviour {
+
+    public GameObject EggTop,
+                      EggBottom;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.tag == "Ground")
+        {
+            //TODO GAMESTATE
+            GameObject.Destroy(this.gameObject);
+            GameObject go = (GameObject)Instantiate(EggTop);
+            go.transform.position = this.gameObject.transform.position;
+            GameObject go2 = (GameObject)Instantiate(EggBottom);
+            go2.transform.position = this.gameObject.transform.position;
+        }
+    }
+}
