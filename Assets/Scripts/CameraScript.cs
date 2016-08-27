390d0428fdf82563d577af99179cc387ab53253b
thomz12 <thomz12@hotmail.nl>
2016-08-27 11:44:37 +0200

    Added camera follw

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+    public Transform Target;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Target)
+        {
+            Vector3 velocity = Vector3.zero;
+            transform.position = Vector3.SmoothDamp(transform.position, new Vector3(Target.position.x, transform.position.y, transform.position.z), ref velocity, 3.0f * Time.deltaTime);
+        }
+	}
+}
