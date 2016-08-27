1a54f86e6253714c2628bf7a7bffc86973a9749b
thomz12 <thomz12@hotmail.nl>
2016-08-27 22:22:06 +0200

    Added Sprint

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class Player : MonoBehaviour {
 
     public float speed;
+    public float sprintMultiplier;
 
 	// Use this for initialization
 	void Start ()
@@ -14,8 +15,13 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
+        float actualSpeed = speed;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+            actualSpeed *= sprintMultiplier;
+
         Rigidbody2D body = GetComponent<Rigidbody2D>();
-        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * actualSpeed, body.velocity.y);
 
         GetComponent<Animator>().SetFloat("MovementSpeed", body.velocity.x);
 	}
