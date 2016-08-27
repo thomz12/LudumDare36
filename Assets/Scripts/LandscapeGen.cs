63225410e6d16b04baac6e869b55c164487b4a7b
remco96 <remcostolwijk1996@gmail.com>
2016-08-27 11:18:14 +0200

    World spawn
    TODO: remove old world

---
+++ Assets/Scripts/LandscapeGen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LandscapeGen : MonoBehaviour {
+
+    public GameObject Landscape;
+    private int spawnCount = 0;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameObject.FindGameObjectWithTag("MainCamera").transform.position.x >= 190 + 480*spawnCount)
+        {
+            GameObject go = (GameObject)Instantiate(Landscape);
+            spawnCount++;
+            go.transform.position = new Vector3(480 * spawnCount, 0, 0);
+            
+        }
+	}
+}
